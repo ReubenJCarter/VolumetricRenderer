6119633b485cee3b771fdd185b8077e2c70862ff
Reuben Carter <reubenjcarter@gmail.com>
2018-06-05 23:40:19 +0100

    Added To single Tiff file loader with directories

--- src/IO/Image3DFromTIFFFile.cpp
+++ src/IO/Image3DFromTIFFFile.cpp
@@ -64,5 +64,78 @@ bool Image3DFromTIFFFileSequence(Image3D* image, std::vector<std::string> fileNa
 
 bool Image3DFromTIFFFile(Image3D* image, std::string fileName)
 {
-	return false; 
+	 
+	TIFF* tif = TIFFOpen(fileName.c_str(), "r");
+		
+	if (!tif) 
+	{
+		std::cout << "Image3DFromTIFFFile:" << "failed to open file" << std::endl; 
+		return false; 
+	}
+	
+	uint32_t width = 0;
+	uint32_t height = 0; 
+	bool firstRun = true;
+	
+	int depth = 0;
+	
+	while(TIFFReadDirectory(tif))
+	{
+	
+		uint32_t w, h;
+		TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
+		TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
+		if(firstRun)
+		{
+			width = w;
+			height = h;
+			firstRun = false; 
+		}
+		else
+		{
+			if(w != width || h != height)	
+			{
+				std::cerr << "Image3DFromTIFFFile: Images not the same size" << fileName << std::endl;
+				return false; 
+			}
+		}
+		depth++; 
+	}
+	
+	TIFFClose(tif);
+	
+	
+	//Check if images have some wifdth and height
+	if(width <= 0 || height <= 0)
+	{
+		std::cerr << "Image3DFromTIFFFile: Images have zero size" << std::endl;
+		return false;
+	}
+	
+	//Allocate Image
+	image->Allocate(width, height, depth, 4);
+	
+	//Load Data
+	tif = TIFFOpen(fileName.c_str(), "r");
+	
+	if (!tif) 
+	{
+		std::cout << "Image3DFromTIFFFile:" << "failed to open file second time around" << std::endl; 
+		return false; 
+	}
+
+	int i = 0;
+	while(TIFFReadDirectory(tif))
+	{
+		unsigned char* imageData = (unsigned char*)image->Data() + width * height * 4 * i;
+		TIFFReadRGBAImage(tif, width, height, (uint32_t*)imageData, 0);
+		i++;
+	}
+	
+	TIFFClose(tif);
+
+	
+	return true; 
+	
+	
 }
